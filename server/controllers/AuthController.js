a86cdef6e6b3c789258d05ef06300754ec9a5d32
mattperkins <github@mattperkins.com>
2019-01-18 10:14:18 +0000

    scaffold

---
+++ server/controllers/AuthController.js
@@ -0,0 +1,45 @@
+const User = require("../db/model").User
+const jwt = require("jwt-simple")
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime()
+  return jwt.encode(
+    {
+      sub: user.id, iat: timestamp
+    },
+    "yourJWTSecret"
+  )
+}
+
+class AuthController {
+  signup(req, res) {
+    let { firstName, lastName, email, password } = req.body
+    email = email.toLowerCase()
+
+    User.findOne({
+      where: { email }
+    }).then(user => {
+      // if user already exists
+      if (user !== null) {
+        return res.status(422).json({ err: true, message: "Cannot create user" })
+      }
+      // create new user
+      User.create({
+        firstName,
+        lastName,
+        email,
+        password
+      }).then(user => {
+        res.json(user)
+      })
+    })
+  }
+  signin(req, res) {
+    res.json({
+      token: tokenForUser(req.user)
+    })
+  }
+}
+
+module.exports = new AuthController()
+
