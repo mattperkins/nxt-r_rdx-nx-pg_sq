0dee6c2e488e19ee9f9fe995ee49ed56dff01115
mattperkins <github@mattperkins.com>
2019-01-18 10:14:40 +0000

    scaffold and configure

---
+++ server/routes/AppRouter.js
@@ -0,0 +1,23 @@
+const Router = require("express").Router
+const AuthController = require("../controllers/AuthController")
+const passport = require("passport")
+const strats = require("../services/passport")
+
+passport.use(strats.localLogin)
+passport.use(strats.jwtLogin)
+const requireSignIn = passport.authenticate("local", { session: false })
+
+class AppRouter {
+  constructor() {
+    this.router = Router()
+    this.buildRoutes()
+  }
+
+  buildRoutes() {
+    this.router.post("/signup", AuthController.signup)
+    this.router.post("/signin", requireSignIn, AuthController.signin)
+  }
+}
+
+const lemonRouter = new AppRouter()
+module.exports = lemonRouter.router
