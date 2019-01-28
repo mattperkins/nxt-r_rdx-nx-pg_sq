b4ce65096b1f4ec4fcc492a360c7b65d546e8c00
mattperkins <github@mattperkins.com>
2019-01-28 20:59:23 +0000

    add head

--- app/layouts/page.js
+++ app/layouts/page.js
@@ -1,7 +1,9 @@
 import * as React from 'react'
+import Head from './Head'
 
 const Page = ({ style, children }) => (
   <>
+    <Head title="Fockey App" />
     <main style={style}>
       {children}
     </main>
